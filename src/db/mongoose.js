df154f0d80773d11817bba39370e5b2ba96668e5
IwaLain <arthes.menetil@gmail.com>
2021-04-26 20:32:10 +0300

    prod fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://127.0.0.1:27017/ho4x'
+const url = 'mongodb://93.79.187.251:27017/ho4x'
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
